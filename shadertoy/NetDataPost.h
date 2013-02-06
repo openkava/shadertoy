dbcfe57fddcb79279566a5dad34c968b5480c72a
openkava <openkava@gmail.com>
2013-02-06 21:49:35 +0800

    aaa

--- shadertoy/NetDataPost.h
+++
@@ -1,9 +0,0 @@
-
-//
-@interface NetDataPost : MKNetworkEngine
-
-
-typedef void (^postGetDataBlock)(NSDictionary *dataSet);
--(MKNetworkOperation*) postGetDataWithRequestData: (NSDictionary *) requestData method:(NSString*) method  usingBlockObject:(postGetDataBlock) paramBlockObject;
-
-@end
